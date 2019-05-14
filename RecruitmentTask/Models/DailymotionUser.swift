3d389d441a88437e5810ea1349c02154b6c2a53f
Adam Kędzia <akedzia@egnyte.com>
2019-05-14 21:53:49 +0200

    Created basic user models

---
+++ RecruitmentTask/Models/DailymotionUser.swift
@@ -0,0 +1,14 @@
+//
+//  DailymotionUser.swift
+//  RecruitmentTask
+//
+//  Created by Adam Kędzia on 14/05/2019.
+//  Copyright © 2019 Adam Kędzia. All rights reserved.
+//
+
+import Foundation
+
+struct DailyMotionUser: User {
+    let username: String
+    let avatarUrl: URL
+}
