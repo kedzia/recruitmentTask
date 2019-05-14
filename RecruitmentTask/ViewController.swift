4a26f9b29436abf1eee06f67c20f40976c123be6
Adam Kędzia <akedzia@egnyte.com>
2019-05-14 21:42:01 +0200

    Initial Commit

---
+++ RecruitmentTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecruitmentTask
+//
+//  Created by Adam Kędzia on 14/05/2019.
+//  Copyright © 2019 Adam Kędzia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RecruitmentTaskTests/RecruitmentTaskTests.swift
@@ -0,0 +1,34 @@
+//
+//  RecruitmentTaskTests.swift
+//  RecruitmentTaskTests
+//
+//  Created by Adam Kędzia on 14/05/2019.
+//  Copyright © 2019 Adam Kędzia. All rights reserved.
+//
+
+import XCTest
+@testable import RecruitmentTask
+
+class RecruitmentTaskTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
